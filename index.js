428547459352883f6bf7395597efcccd220a3749
Will McLean <willmcl@gmail.com>
2019-11-20 14:51:17 +1100

    Add all required avatar fields

--- index.js
+++ index.js
@@ -1,89 +1,113 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { pool } = require('./config')
-const helmet = require('helmet')
-const compression = require('compression')
-const rateLimit = require('express-rate-limit')
-const { body, check, validationResult } = require('express-validator')
+const express = require( 'express' )
+const bodyParser = require( 'body-parser' )
+const cors = require( 'cors' )
+const { pool } = require( './config' )
+const helmet = require( 'helmet' )
+const compression = require( 'compression' )
+const rateLimit = require( 'express-rate-limit' )
+const { body, check, validationResult } = require( 'express-validator' )
 
 const isProduction = process.env.NODE_ENV === 'production'
 const origin = {
   origin: isProduction ? 'https://ttc-aag.netlify.com' : '*',
 }
 
-const limiter = rateLimit({
+const limiter = rateLimit( {
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 10, // 10 requests,
-})
+} )
 
-const postLimiter = rateLimit({
+const postLimiter = rateLimit( {
   windowMs: 1 * 60 * 1000,
   max: 10,
-})
+} )
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors(origin))
-app.use(compression())
-app.use(helmet())
-app.use(limiter)
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoded( { extended: true } ) )
+app.use( cors( origin ) )
+app.use( compression() )
+app.use( helmet() )
+app.use( limiter )
 
-const getAvatars = (request, response) => {
-  pool.query('SELECT * FROM avatars', (error, results) => {
-    if (error) {
+const getAvatars = ( request, response ) => {
+  pool.query( 'SELECT * FROM avatars', ( error, results ) => {
+    if ( error ) {
       throw error
     }
-    response.status(200).json(results.rows)
-  })
+    response.status( 200 ).json( results.rows )
+  } )
 }
 
 app
-.route('/avatars')
+.route( '/avatars' )
 // GET endpoint
-.get(getAvatars)
+.get( getAvatars )
 
 // POST endpoint
 app.post(
   '/avatars',
   [
-    check('avatar')
-    .not()
-    .isEmpty()
-    .isInt({ min: 1, max: 2 }),
-    check('skinTone')
-    .not()
-    .isEmpty()
-    .isLength({ min: 5, max: 255 })
-    .trim(),
-    check('jawLine')
-    .not()
-    .isEmpty()
-    .isLength({ min: 5, max: 255 })
-    .trim(),
+    check( 'avatar' ).not().isEmpty().isInt( { min: 1, max: 2 } ),
+    check( 'skinTone' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'jawLine' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'nose' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'beard' ).isLength( { min: 1, max: 255 } ).trim(),
+    check( 'hairColour' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'hairLength' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'hairLine' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'hairTexture' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'eyeColour' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'eyeShape' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'eyeBrows' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'cheek' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'lips' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'participantAge' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'participantOccupation' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
+    check( 'participantLocation' ).not().isEmpty().isLength( { min: 1, max: 255 } ).trim(),
   ],
   postLimiter,
-  (request, response) => {
-    const errors = validationResult(request)
+  ( request, response ) => {
+    const errors = validationResult( request )
 
-    if (!errors.isEmpty()) {
-      return response.status(422).json({ errors: errors.array() })
+    if ( !errors.isEmpty() ) {
+      return response.status( 422 ).json( { errors: errors.array() } )
     }
 
-    const { avatar, skinTone, jawLine } = request.body
+    const {
+      avatar,
+      skinTone,
+      jawLine,
+      nose,
+      beard,
+      hairColour,
+      hairLength,
+      hairLine,
+      hairTexture,
+      eyeColour,
+      eyeShape,
+      eyeBrows,
+      cheek,
+      lips,
+      participantAge,
+      participantOccupation,
+      participantLocation
+    } = request.body
 
-    pool.query('INSERT INTO avatars (avatar, skinTone, jawLine) VALUES ($1, $2, $3)', [avatar, skinTone, jawLine], error => {
-      if (error) {
-        throw error
-      }
-      response.status(201).json({ status: 'success', message: 'Avatar added.' })
-    })
+    pool.query(
+      'INSERT INTO avatars (avatar, skinTone, jawLine, nose, beard, hairColour, hairLength, hairLine, hairTexture, eyeColour, eyeShape, eyeBrows, cheek, lips, participantAge, participantOccupation, participantLocation) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)',
+      [ avatar, skinTone, jawLine, nose, beard, hairColour, hairLength, hairLine, hairTexture, eyeColour, eyeShape, eyeBrows, cheek, lips, participantAge, participantOccupation, participantLocation ],
+      error => {
+        if ( error ) {
+          throw error
+        }
+        response.status( 201 ).json( { status: 'success', message: 'Avatar added.' } )
+      } )
   }
 )
 
 // Start server
-app.listen(process.env.PORT || 3002, () => {
-  console.log(`Server listening`)
-})
+app.listen( process.env.PORT || 3002, () => {
+  console.log( `Server listening` )
+} )
--- init.sql
+++ init.sql
@@ -2,8 +2,41 @@ CREATE TABLE avatars (
   ID SERIAL PRIMARY KEY,
   avatar SMALLINT NOT NULL,
   skinTone VARCHAR(40) NOT NULL,
-  jawLine VARCHAR(40) NOT NULL
+  jawLine VARCHAR(40) NOT NULL,
+  nose VARCHAR(40) NOT NULL,
+  beard VARCHAR(40),
+  hairColour VARCHAR(40) NOT NULL,
+  hairLength VARCHAR(40) NOT NULL,
+  hairLine VARCHAR(40) NOT NULL,
+  hairTexture VARCHAR(40) NOT NULL,
+  eyeColour VARCHAR(40) NOT NULL,
+  eyeShape VARCHAR(40) NOT NULL,
+  eyeBrows VARCHAR(40) NOT NULL,
+  cheek VARCHAR(40) NOT NULL,
+  lips VARCHAR(40) NOT NULL,
+  participantAge VARCHAR(40) NOT NULL,
+  participantOccupation VARCHAR(40) NOT NULL,
+  participantLocation VARCHAR(40) NOT NULL,
+  posting_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-INSERT INTO avatars (avatar, skinTone, jawLine)
-VALUES  (1, 'veryFair', 'v' );
+INSERT INTO avatars (avatar, skinTone, jawLine, nose, beard, hairColour, hairLength, hairLine, hairTexture, eyeColour, eyeShape, eyeBrows, cheek, lips, participantAge, participantOccupation, participantLocation)
+VALUES  (1, 'a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'n', 'o', 'p', 'q', 'r' );
+
+
+ALTER TABLE avatars
+ADD COLUMN nose VARCHAR(40) NOT NULL,
+ADD COLUMN beard VARCHAR(40),
+ADD COLUMN hairColour VARCHAR(40) NOT NULL,
+ADD COLUMN hairLength VARCHAR(40) NOT NULL,
+ADD COLUMN hairLine VARCHAR(40) NOT NULL,
+ADD COLUMN hairTexture VARCHAR(40) NOT NULL,
+ADD COLUMN eyeColour VARCHAR(40) NOT NULL,
+ADD COLUMN eyeShape VARCHAR(40) NOT NULL,
+ADD COLUMN eyeBrows VARCHAR(40) NOT NULL,
+ADD COLUMN cheek VARCHAR(40) NOT NULL,
+ADD COLUMN lips VARCHAR(40) NOT NULL,
+ADD COLUMN participantAge VARCHAR(40) NOT NULL,
+ADD COLUMN participantOccupation VARCHAR(40) NOT NULL,
+ADD COLUMN participantLocation VARCHAR(40) NOT NULL,
+ADD COLUMN posting_date DATE NOT NULL DEFAULT CURRENT_DATE;
